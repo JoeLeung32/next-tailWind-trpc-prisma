c3c8aad0e83b1f8639caf3230684f969479dbdde
Joe Leung <joeleung32@yahoo.com.hk>
2023-03-15 22:53:50 +0800

    API connection test

--- apps/nx-next/pages/index.tsx
+++ apps/nx-next/pages/index.tsx
@@ -1,16 +1,45 @@
 import styles from './index.module.css';
+import * as process from "process";
 
-export function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
-  return (
-    <div className={styles.page}>
-      <h1>Hello Joe!</h1>
-    </div>
-  );
+export async function getServerSideProps(context) {
+    const apiPath = `tutorials?fields=title,headline,publishedAt,scheduleToPublishAt`
+    const apiURL = `${process.env.apiUrl}${apiPath}`
+    const res = await fetch(apiURL, {
+        headers: {
+            Authorization: `Bearer ${process.env.apiToken}`
+        }
+    })
+    const data = await res.json()
+    console.log('~>', data)
+    return {
+        props: {
+            data: data
+        }
+    }
+}
+
+export function Index(props) {
+    const {data} = props
+    const total = data?.meta?.pagination?.total || 0
+    const list = data?.data || []
+    const error = data?.error || null
+    return (
+        <div className={styles.page}>
+            <h1>Hello Joe!</h1>
+            <p>Total: {total}</p>
+            {list && list.length ? (
+                list.map(({id, attributes}) => (
+                    <div key={`ele${id}`}>{attributes.title}</div>
+                ))
+            ) : (
+                <div>
+                    <h1>{error.status}</h1>
+                    <p>{error.message}</p>
+                    <small>{error.name}</small>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Index;
