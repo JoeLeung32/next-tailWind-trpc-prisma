afe6f63359285abed3df901d87e0c72d247c1db7
Joe Leung <joeleung32@yahoo.com.hk>
2023-03-17 01:16:57 +0800

    Google font test

--- apps/nx-next/pages/_app.tsx
+++ apps/nx-next/pages/_app.tsx
@@ -1,19 +1,30 @@
-import Head from 'next/head';
-import {AppProps} from 'next/app';
-import {trpc} from '../utils/trpc';
-import './styles.css';
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import { trpc } from '../utils/trpc'
+import { SiteProvider } from '../contexts/site'
+import './styles.css'
+import { inter, notoSansTC, nunito, rubik } from '../constants/fonts'
 
-function CustomApp({Component, pageProps}: AppProps) {
+const fontGoogle = `${inter.variable} ${notoSansTC.variable} ${nunito.variable} ${rubik.variable}`
+
+function WebApp({ Component, pageProps }: AppProps) {
     return (
-        <>
-            <Head>
-                <title>Welcome to nx-next!</title>
-            </Head>
-            <main className="app">
-                <Component {...pageProps} />
-            </main>
-        </>
-    );
+        <SiteProvider>
+            <>
+                <Head>
+                    <title>Learnbook</title>
+                    <meta name="description" content="" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1"
+                    />
+                </Head>
+                <main className={`app ${fontGoogle} font-sans`}>
+                    <Component {...pageProps} />
+                </main>
+            </>
+        </SiteProvider>
+    )
 }
 
-export default trpc.withTRPC(CustomApp);
+export default trpc.withTRPC(WebApp)
