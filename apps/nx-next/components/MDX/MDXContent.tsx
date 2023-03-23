5208c140df5239efb45e37da680188de15762e90
Joe.C.LEUNG <joe.c.leung@accenture.com>
2023-03-23 17:53:27 +0800

    Remark support sort break eg: char(10) / <br>

--- apps/nx-next/components/MDX/MDXContent.tsx
+++ apps/nx-next/components/MDX/MDXContent.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { remark } from 'remark'
 import html from 'remark-html'
+import breaks from 'remark-breaks'
 import styles from './content.module.css'
 
 const MDXContent = ({ content }: { content: string }) => {
     const [mdx, setMdx] = React.useState<string | null>(null)
     const fetchData = async () => {
-        const { value } = await remark().use(html).process(content)
+        const { value } = await remark().use(breaks).use(html).process(content)
         setMdx(value.toString())
     }
     React.useEffect(() => {
