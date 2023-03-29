ee58c529b796a416ab9f773d9ce6d19ddf1003de
Joe.C.LEUNG <joe.c.leung@accenture.com>
2023-03-29 11:53:19 +0800

    BackButton enhancement

--- apps/nx-next/components/BackButton/BackButton.tsx
+++ apps/nx-next/components/BackButton/BackButton.tsx
@@ -3,13 +3,27 @@ import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 
 const BackButton = ({ text }: { text?: string | null }) => {
-    const { t } = useTranslation()
+    const { t, i18n } = useTranslation()
     const router = useRouter()
+    const goBack = async () => {
+        const url = router.pathname.split('/').slice(0, -1).join('/')
+        if (window.history.state && window.history.state.idx > 0) {
+            router.back()
+        } else {
+            await router.replace(
+                {
+                    pathname: url
+                },
+                undefined,
+                { locale: i18n.language }
+            )
+        }
+    }
     return (
         <div className={`my-5`}>
             <button
                 type={`button`}
-                onClick={() => router.back()}
+                onClick={() => goBack()}
                 className={`underline`}
             >
                 {text || t('Back to previous page')}
